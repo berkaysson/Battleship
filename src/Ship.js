48d9017cc0a83db95673e90b95e9ce8a3dfedd8b
berkaysson <berkaysonel85@gmail.com>
2023-02-08 19:13:38 +0300

    A Ship class with length constructor, isSunk and hit methods.

---
+++ src/Ship.js
@@ -0,0 +1,17 @@
+class Ship {
+    constructor(length){
+        this.length = length;
+        this.#hits = 0;
+    }
+
+    hit(){
+        this.hits++;
+    }
+
+    isSunk(){
+        if(this.hits === this.length) return true;
+        else return false;
+    }
+}
+
+export default Ship;
