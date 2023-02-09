46efb5230a772a2ab8d3d471ea56cfc891409d70
berkaysson <berkaysonel85@gmail.com>
2023-02-09 14:36:05 +0300

    Installed babel and made neccessary configs.

--- webpack.config.js
+++ webpack.config.js
@@ -7,43 +7,18 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-};
-module.exports = {
-  mode: "development",
-};
-module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: ["node_modules"],
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.gif$/,
-        type: "asset/inline",
-      },
-      {
-        test: /\.(ttf|eot|svg)$/,
-        type: "asset/resource",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
     ],
   },
-  resolve: {
-    alias: {
-      config$: "./configs/app-config.js",
-      react: "./vendor/react-master",
-    },
-    extensions: [".js", ".jsx"],
-    modules: [
-      "node_modules",
-      "bower_components",
-      "shared",
-      "/shared/vendor/modules",
-    ],
-  },
+};
+module.exports = {
+  mode: "development",
 };
