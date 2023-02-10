05f140201a303879928bea08c238cf582a03cb00
berkaysson <berkaysonel85@gmail.com>
2023-02-10 20:39:26 +0300

    Player factory function with methods of gameboard, name, attack and checkWinner.

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,11 +1,36 @@
 import Gameboard from "./Gameboard"
 
-const Player = () => {
+const Player = (name) => {
   let gameboard = Gameboard(10);
 
-  return {
-    gameboard
+  function attack(enemy, hitPos){
+    if(checkEnemy(enemy)){
+      enemy.gameboard.receiveAttack(hitPos);
+    }
+    else return 'Invalid Player.'
   }
+
+  function checkWin(enemy){
+    if(checkEnemy(enemy)){
+      if(!enemy.gameboard.isShipLeft()) return true;
+      else return false;
+    }
+    else return 'Invalid Player.'
+  }
+
+  function checkEnemy(enemy){
+    if(enemy instanceof Object){  // find a way to check 'enemy' is a Player
+      return true
+    }
+    else return false
+  }
+
+  return {
+    gameboard,
+    name,
+    attack,
+    checkWin
+  };
 }
 
 export default Player;
