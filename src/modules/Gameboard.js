e249dc19627a4d01e1da65bda40b798feeb4e290
berkaysson <berkaysonel85@gmail.com>
2023-02-10 20:38:25 +0300

    Format document.

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,51 +1,50 @@
-import Ship from "./Ship";
-
 const Gameboard = (size) => {
   let board = createBoard(size); // 10x10 gameboard
 
-  function placeShip(startPos, dir = 'hor', ship) {
+  function placeShip(startPos, dir = "hor", ship) {
     let endPos;
-    if (dir === "ver") endPos = [startPos[0], startPos[1] + ship.length - 1];  
-    else if (dir === "hor") endPos = [startPos[0] + ship.length - 1, startPos[1]];
-    else return 'Direction is nor correct.'
+    if (dir === "ver") endPos = [startPos[0], startPos[1] + ship.length - 1];
+    else if (dir === "hor")
+      endPos = [startPos[0] + ship.length - 1, startPos[1]];
+    else return "Direction is nor correct.";
 
-    if(isInBoard(startPos) && isInBoard(endPos)){
+    if (isInBoard(startPos) && isInBoard(endPos)) {
       for (let x = startPos[0]; x <= endPos[0]; x++) {
         for (let y = startPos[1]; y <= endPos[1]; y++) {
-          if(isEmpty([x,y])) board[x][y] = ship;
-          else return 'The position is used.'
+          if (isEmpty([x, y])) board[x][y] = ship;
+          else return "The position is used.";
         }
       }
-    }
-    else{
-      return 'Invalid position.'
+    } else {
+      return "Invalid position.";
     }
   }
 
   function receiveAttack(hitPos) {
-    if(isInBoard(hitPos)){
-      if(board[hitPos[0]][hitPos[1]] === 'Hit!') return 'This position has been hitted.'
-      else if(board[hitPos[0]][hitPos[1]] instanceof Object){
+    if (isInBoard(hitPos)) {
+      if (board[hitPos[0]][hitPos[1]] === "Hit!")
+        return "This position has been hitted.";
+      else if (board[hitPos[0]][hitPos[1]] instanceof Object) {
         board[hitPos[0]][hitPos[1]].hit();
-        if(board[hitPos[0]][hitPos[1]].isSunk()){
-          board[hitPos[0]][hitPos[1]] = 'Hit!';
-          return 'Ship Sunk!'
+        if (board[hitPos[0]][hitPos[1]].isSunk()) {
+          board[hitPos[0]][hitPos[1]] = "Hit!";
+          return "Ship Sunk!";
         }
-        board[hitPos[0]][hitPos[1]] = 'Hit!';
+        board[hitPos[0]][hitPos[1]] = "Hit!";
+      } else if (board[hitPos[0]][hitPos[1]] == null) {
+        board[hitPos[0]][hitPos[1]] = "Miss!";
       }
-      else if(board[hitPos[0]][hitPos[1]] == null){
-        board[hitPos[0]][hitPos[1]] = 'Miss!';
-      }
-    }
-    else return 'Invalid position.'
+    } else return "Invalid position.";
   }
 
-  function isShipLeft(){
+  function isShipLeft() {
     for (let x = 0; x < board.length; x++) {
       for (let y = 0; y < board.length; y++) {
-        if(board[x][y] instanceof Object){return true}
+        if (board[x][y] instanceof Object) {
+          return true;
+        } // board[x][y] should be equal to a Ship instaces, find a way to check it
+      }
     }
-  }
     return false;
   }
 
@@ -61,8 +60,8 @@ const Gameboard = (size) => {
     return true;
   }
 
-  function isEmpty(position){
-    if(board[position[0]][position[1]] == null) return true;
+  function isEmpty(position) {
+    if (board[position[0]][position[1]] == null) return true;
     else return false;
   }
 
