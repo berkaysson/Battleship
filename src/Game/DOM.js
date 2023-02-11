f5369aba17e837aa9e3928fec853995ab1e05364
berkaysson <berkaysonel85@gmail.com>
2023-02-11 18:42:01 +0300

    DOM file.

---
+++ src/Game/DOM.js
@@ -0,0 +1,8 @@
+export default {
+  player1Info: document.getElementById("player1-info"),
+  player1Gameboard: document.getElementById("player1-gameboard"),
+  player2Info: document.getElementById("player2-info"),
+  player2Gameboard: document.getElementById("player2-gameboard"),
+  placementBoard: document.getElementById("placement-board"),
+  placementBtn: document.getElementById("placement-btn")
+};
