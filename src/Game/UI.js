6830beb6f28c64bc238cc03cad1e63db61440ef6
berkaysson <berkaysonel85@gmail.com>
2023-02-12 19:32:39 +0300

    A game object defined by using Game.js, a method initButtons to initiate events of buttons, method of updateBoard to update given div of board and initPlacementBoard made functional.

--- src/Game/UI.js
+++ src/Game/UI.js
@@ -1,5 +1,5 @@
 import Game from "./Game";
-import elements from "./DOM"
+import elements from "./DOM";
 
 const {
   player1Info,
@@ -8,41 +8,52 @@ const {
   player2Gameboard,
   placementBoard,
   placementBtn,
+  rotateBtn,
+  shipDirInfo,
+  shipLengthInfo
 } = elements;
 
 export default class UI{
-  static render(){  // it will be called in index.js when the page loaded
+  static game = Game();
+  
+
+  static render(){
     UI.initPlacementBoard();
+    UI.initButtons();
   }
 
-  static initPlacementBoard(){  // should create gameboard 10x10
+  static initPlacementBoard(){
     for(let row = 0; row < 10; row++){
       for(let col = 0; col < 10; col++){
         let grid = document.createElement("div");
         grid.className = "grid";
         grid.id = `grid-${row}-${col}`;
-        grid.addEventListener("click", () => {  // every grid must have event of UI.placeShip()
-          Game.player1.gameboard.placeShip([row, col], 'hor', Game.ships[0]);
-          UI.updateBoard()
+        grid.addEventListener("click", () => {
+          if (UI.game.placeShipsPerson([
+              row,
+              col,
+              rotateBtn.getAttribute("data-direction")]) !== false) 
+          {
+            if(UI.game.currentShipLength()) shipLengthInfo.textContent = UI.game.currentShipLength();
+          }
+          UI.updateBoard(placementBoard);
         });
         placementBoard.appendChild(grid);
       }
     }
-    // add a rotate button to choose placement will be 'ver' or 'hor'
     // later can be add feature of removing placed ships
     // add eventlistener to Approve button in placement modal, if all ships are placed correctly then approve button will be available
-    // when clicked approve button call initGameboard()
+    // when clicked approve button call initGameboard(), when clicked approvebutton also AI should place its ships
   }
 
-  static placeShip(target){
-    // should take target and convert it a position (ex. [0, 0])
-    // should check if target position available, if it is, will place ship to players gameboard (also will change that grids style), if not, will give a warning
-  }
-
-  static updateBoard(){
-    //this will be called every move in both gameboard
-    //should update board
-    //can initGameboard() be used for this
+  static updateBoard(boardDiv){
+    const children = boardDiv.children;
+    for (let child of children) {
+      if (
+        UI.game.player1.gameboard.board[Number(child.id[5])][Number(child.id[7])] instanceof Object) {
+        child.classList.add("ship");
+      }
+    }
   }
 
   static initGameboard(){
@@ -50,5 +61,18 @@ export default class UI{
     // every grid of player2 will have events(click) of: Gameboard.receivAttack, Game.attackAI, (maybe makeTurn), with this every attack of Person will be met with attack of AI(before attack of Aı and person, it should be checked if someone wins)
     // every grid of board of player1 will have style acc to null or ship and also hit or miss
     // every grid of board of player2(AI) will have style acc to hit or miss
+  }
+
+  static initButtons(){
+    rotateBtn.addEventListener("click", () => {
+      let currentDirection = rotateBtn.getAttribute("data-direction");
+      if (currentDirection === "hor") {
+        rotateBtn.setAttribute("data-direction", "ver");
+        shipDirInfo.textContent = "Vertically";
+      } else {
+        rotateBtn.setAttribute("data-direction", "hor");
+        shipDirInfo.textContent = "Horizantally";
+      }
+    });
   }
 }
