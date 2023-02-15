49b7cbc70ccc4d398626581ad9dca6649111914e
berkaysson <berkaysonel85@gmail.com>
2023-02-15 18:42:34 +0300

    ID and getID methods added.

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,9 +1,11 @@
 const Ship = (length) => {
   let hits = 0;
+  const id = `shipID-${length}-${Date.now()}${Math.floor(Math.random() * 10)}`
 
   return {
     hit: () => hits++,
     isSunk: () => hits === length,
+    getID: () => id,
     length,
   };
 };
