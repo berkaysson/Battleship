cd07ff1e64cf029ea79fd23609cc64f10f83813b
berkaysson <berkaysonel85@gmail.com>
2023-02-15 18:42:51 +0300

    Update on DOM.

--- src/Game/DOM.js
+++ src/Game/DOM.js
@@ -5,8 +5,10 @@ export default {
   player2Gameboard: document.getElementById("player2-gameboard"),
   placementForm: document.getElementById("placement-form"),
   placementBoard: document.getElementById("placement-board"),
+  placementInfo: document.getElementById("placement-info"),
   approveBtn: document.getElementById("placement-approve-btn"),
   rotateBtn:  document.getElementById("rotate-placement-btn"),
+  removeBtn:  document.getElementById("remove-placement-btn"),
   shipDirInfo: document.getElementById("ship-direction"),
   shipLengthInfo: document.getElementById("ship-length"),
 };
