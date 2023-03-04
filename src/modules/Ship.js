d8372edcead0d346c78c28551e8e1199c313be8f
berkaysson <berkaysonel85@gmail.com>
2023-03-04 22:45:33 +0300

    Fix to ship ID and feat color of ship

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,11 +1,15 @@
 const Ship = (length) => {
   let hits = 0;
-  const id = `shipID-${Math.floor(Math.random() * 10000)}`;
+  const id = `shipID-${length}-${Math.floor(Math.random() * 10000)}`;
+  const color = `rgb(${Math.floor(Math.random() * 256)}, ${
+    Math.floor(Math.random() * 256)
+  }, ${Math.floor(Math.random() * 256)})`;
 
   return {
     hit: () => hits++,
     isSunk: () => hits === length,
     getID: () => id,
+    getColor: () => color,
     length,
   };
 };
