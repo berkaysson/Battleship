8f6f194808ae2ce58fca38707efda06235b90573
berkaysson <berkaysonel85@gmail.com>
2023-03-13 22:48:46 +0300

    Install webpack server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+// import UI from "./Game/UI";
+// import "./style/style.css";
+
+// UI.render();
+
 import UI from "./Game/UI";
 import "./style/style.css";
 
-UI.render();
+const render = () => {
+  UI.render();
+};
+
+if (module.hot) {
+  module.hot.accept("./Game/UI", () => {
+    render();
+  });
+
+  module.hot.accept("./style/style.css", () => {
+    const link = document.querySelector('link[href^="./style/style.css"]');
+    link.href = "./style/style.css" + "?ver=" + new Date().getTime();
+  });
+}
+
+render();
